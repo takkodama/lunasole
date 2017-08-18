ce85705b80b59c217571258c022c781518f4efa7
takschatzl <kodama.takumi.wp@alumni.tsukuba.ac.jp>
2017-08-18 23:14:05 +0900

    allowing post from external request

--- app/controllers/temps_controller.rb
+++ app/controllers/temps_controller.rb
@@ -4,4 +4,11 @@ class TempsController < ApplicationController
     @temps = Temp.all.order(yyyymmddhh: 'asc')
   end
 
+  def create
+    # save
+    @temps = Temp.new(params.require(:post).permit(:degree, :yyyymmddhh))
+    @temps.save
+    # redirect
+    redirect_to temps_path
+  end
 end
